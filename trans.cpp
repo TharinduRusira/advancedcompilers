8b08d5876c64b5cf632869d132f6cef488890ecb
Tharindu Rusira <tharindurusira@gmail.com>
2016-09-18 10:36:49 -0600

    subtract done, needs testing

--- trans.cpp
+++ trans.cpp
@@ -1,17 +1,12 @@
 /*
 Tharindu Rusira (u1011765)
 
-Abstract transfer function for unsigned subtraction of intervals (x,y) where x,y are 4-bit integers 
-The subtraction operation is defined by [x1,x2]-[y1,y2] = [x1-y2,x2-y1]
-https://en.wikipedia.org/wiki/Interval_arithmetic#Simple_arithmetic
-
-Since 4-bit integers are used, 0<x,y<15 and we are required to wrap around underflows(negetive values), overflows will not occur because the subtracted result is always less than or equal to 15. 
-
+Abstract transfer functions for 5-bit signed interval subtraction and 
 */
 
 #include <iostream>
 #include <assert.h>
-
+using namespace std;
 
 class interval{
 
@@ -25,32 +20,38 @@ class interval{
 };
 
 
-void sub(interval x, interval y){
-// let's do a quick sanity check to make sure we are getting 5-bit unsigned integers
-assert(x.lo>=-16 || x.lo<=15 || x.hi>=-16 || x.hi<=15 || y.lo>=-16 || y.lo<=15 || y.hi>=-16 || y.hi<=15 || x.lo<x.hi || y.lo<y.hi);
+interval interval::subtract(interval x, interval y){
+	// let's do a quick sanity check to make sure we are getting 5-bit unsigned integers
+	assert(x.lo>=-16 || x.lo<=15 || x.hi>=-16 || x.hi<=15 || y.lo>=-16 || y.lo<=15 || y.hi>=-16 || y.hi<=15 || x.lo<x.hi || y.lo<y.hi);
 	int a,b;
+	interval result;
 	// we do the subtraction y-x
 	a = y.lo-x.hi;
 	b = x.lo - y.hi;
 
-	if(a<0){
-		a=0; //rounding to 0, closest value in the range [0,15]
-	}
-	if(b<0 || b<a){
-		b=15; //rounding to 15, closest value in the range [0,15]
-	}
-/* If a, the lower bound underflows, eg. (-2,5), we approximate the lowerbound to 0, which gives us the interval (0,5)*/
-/* If b, the upper bound underflows or less than a valid lowerbound, eg. (2,-1),  we approximate the upper bound to 15, which gives us the interval (2,15) */
-	printf("(%d,%d)-(%d,%d)=(%d,%d)\n",x1,x2,y1,y2,a,b);
+	/*
+	 * We are rounding the values that are outside our abstract domain [-16,15] so that there will be no undefined behavior
+	 */
+
+	if(a<-16){a=16- a%16;}
+	if(b>15){b=-16-b%16;}
+	//if a>b after rounding, the resulting interval is invalid
+	assert(a<=b);
+
+	result.lo = a;
+	result.hi = b;
+	cout << "(%d,%d)-(%d,%d)=(%d,%d)\n" << y.lo,y.hi,x.lo,x.hi,a,b;
+	return result;
 }
 
+interval interval::bitwise_and(interval x, interval y){
 
-void bitwise_and(interval x, interval y){
-assert(x.lo>=-16 || x.lo<=15 || x.hi>=-16 || x.hi<=15 || y.lo>=-16 || y.lo<=15 || y.hi>=-16 || y.hi<=15 || x.lo<x.hi || y.lo<y.hi);
+	assert(x.lo>=-16 || x.lo<=15 || x.hi>=-16 || x.hi<=15 || y.lo>=-16 || y.lo<=15 || y.hi>=-16 || y.hi<=15 || x.lo<x.hi || y.lo<y.hi);
+	interval result;
 
 
 
-
+return result;
 }
 
 
@@ -62,9 +63,7 @@ assert(x.lo>=-16 || x.lo<=15 || x.hi>=-16 || x.hi<=15 || y.lo>=-16 || y.lo<=15 |
 
 int main(void){
 //test cases
-	sub(4,5,1,2);
-	sub(1,2,3,4);
-	sub(5,6,7,8);
+	
 
 return 0;
 }
