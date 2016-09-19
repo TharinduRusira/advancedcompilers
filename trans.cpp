caa190591047f24ccd0ab09d55fed2d4acee2c42
Tharindu Rusira <tharindurusira@gmail.com>
2016-09-19 16:38:08 -0600

    bitwise-and tests added

--- trans.cpp
+++ trans.cpp
@@ -65,14 +65,25 @@ int main(void){
 
 interval i1(10,12); interval i2(0,7);
 interval i3(-10,-5); interval i4(-15,0);
-interval i5(-12,15);
+interval i5(-12,15); interval i6(-3,-4);
 
+cout << "Interval subtract"<<endl;
 //subtract test cases
-//interval::subtract(i2,i1);
-//interval::subtract(i3,i4);
-//interval::subtract(i5,i1);
+interval::subtract(i2,i1);
+interval::subtract(i3,i4);
+interval::subtract(i5,i1);
+interval::subtract(i4,i1);
+interval::subtract(i5,i6);
+interval::subtract(i2,i6);
 
+cout << "Interval bitwise-AND"<<endl;
 //bitwise-AND test cases
-cout << (-10 & 0);
+interval::bitwise_and(i1,i2);
+interval::bitwise_and(i3,i6);
+interval::bitwise_and(i1,i3);
+interval::bitwise_and(i1,i4);
+interval::bitwise_and(i6,i5);
+//interval::bitwise_and();
+
 return 0;
 }
