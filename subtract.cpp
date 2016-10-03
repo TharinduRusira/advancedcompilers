fe75d7d25fabd9a25a8dd3014eaf654bfcb56e0e
Tharindu Rusira <tharindurusira@gmail.com>
2016-10-03 10:03:31 -0600

    fixed subtract transfer function

--- subtract.cpp
+++ subtract.cpp
@@ -5,14 +5,13 @@ interval interval::operator-(const interval &y){
 	a = lo-y.hi;
 	b = hi - y.lo;
 	/*
-	 * We are rounding the values that are outside our abstract domain [-32,31] so that there will be no undefined behavior
+	 * We are rounding the intervals that are outside our abstract domain [-32,31] so that there will be no undefined behavior
 	 */
-	if(a<MIN){a=-MIN + a%(-MIN);}
-	else if(a>MAX){a = -MAX + a%(-MAX);}
-	if (b>MAX){b= -MAX + b%(-MAX);}
-	else if(b<MIN){b= -MIN + b%(-MIN);}
-	//if a>b after wrapping around, return TOP
-	if(b<a){return top();}
+	if(a<MIN && b> MAX){a= a + RANGE; b= b-RANGE;}
+	else if(a<MIN && b<MIN){a= a+RANGE;b=b+RANGE;}
+	else if(a>MAX && b>MAX){a=a-RANGE;b=b-RANGE;}
+	else if((a<MIN && b<MAX && b>MIN)||(b>MAX && a>MIN && a<MAX)){return top();}
+   	if(b<a){return top();}
 	return interval(a,b) ;
 }
 
